6522a773486a5a8814da34461088b8d674a42089
pulkitbajaj <pulkitbajaj277@gmail.com>
2024-09-12 23:57:28 +0530

    Add Comments

--- Audio.java
+++ Audio.java
@@ -5,23 +5,32 @@ import java.util.Scanner;
 import javax.sound.sampled.*;
 
 public class Audio {
-    public static void main(String[] args) throws LineUnavailableException, IOException, UnsupportedAudioFileException {
+    public static void main(String[] args) throws LineUnavailableException /*(if Audio line is unavailable) */, IOException /*(if an I/O error occurs) */, UnsupportedAudioFileException /*(If audio file is not supported) */ {
 
+        // Creating a scanner to read user input
         Scanner scanner = new Scanner(System.in);
+
+        // Load the audio file
         File file = new File("Running It Down - Everet Almond.wav ");
         AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
 
+        // Create a clip to play the audio
         Clip clip = AudioSystem.getClip();
         clip.open(audioStream);
 
 
+        // Initialize the response variable
         String response = "";
+
+        // Loop until the user quits
         while(!response.equals("Q")){
             System.out.println("P = play, S = Stop, R = Reset, Q = Quit");
             System.out.println("Enter your choice");
             response = scanner.next();
             response = response.toUpperCase();
 
+
+            // Handle user input
             switch (response){
                 case ("P"): clip.start();
                 break;
